680e14c3e4757f5d2d45d13d5b3ed3e49f858fb2
pablomorera <mopablo50@yahoo.com>
2014-10-18 20:15:41 -0400

    Add position function

--- flappyturtle/player/turtle.py
+++ flappyturtle/player/turtle.py
@@ -3,6 +3,7 @@ import pygame
 
 
 class Turtle(object):
+    swim_speed = 1
     def __init__(self):
         super().__init__()
         self.image_path = 'sprites/turtle.png'
@@ -17,9 +18,11 @@ class Turtle(object):
     def position(self):
         return [self.pos_x, self.pos_y]
 
-    def update(self):
-        pass
+    def update(self, time):
+        self.pos_y = self.pos_y + self.pos_y *time
 
-    def jump(self):
-        pass
-
+    def swim(self, up, down):
+        if up == True:
+            self.y_vel += Turtle.swim_speed
+        else:
+            self.y_vel -= Turtle.swim_speed
