829f285167b815df7e7af8b196c0fcfbc038043d
pablomorera <mopablo50@yahoo.com>
2014-10-18 21:17:31 -0400

    World keeps on putting turtle down

--- flappyturtle/world.py
+++ flappyturtle/world.py
@@ -5,10 +5,12 @@ __author__ = 'neil@everymundo.com'
 
 
 class World():
-    def __init__(self):
+    def __init__(self, width, height):
         super().__init__()
         self.turtle = None
         self.obstacles = []
+        self.width = width
+        self.height = height
 
     def add_player(self, position):
         self.turtle = Turtle(position)
@@ -17,3 +19,9 @@ class World():
     def add_obstacle(self, obstacle_type):
         if obstacle_type == 'coral':
             self.obstacles.append(Obstacle('sprites/coral.png'))
+    def update(self, time):
+        turtle_bounds = self.turtle.bounds()
+        if turtle_bounds[1] <= 0:
+            self.turtle.toohigh(0)
+        if turtle_bounds[3] >= self.height:
+            self.turtle.toolow(self.height)
