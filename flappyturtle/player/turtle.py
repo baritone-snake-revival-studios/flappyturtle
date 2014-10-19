ff9649d8307ac2ff9827e701d5c92c21fe177794
Neil Goldman <neilgoldman305@gmail.com>
2014-10-18 20:44:36 -0400

    Turtle can scale

--- flappyturtle/player/turtle.py
+++ flappyturtle/player/turtle.py
@@ -9,10 +9,12 @@ class Turtle(object):
         super().__init__()
         self.image_path = 'sprites/turtle.png'
         self.image = pygame.image.load(os.path.join('sprites', 'turtle.png'))
-        self.scale = 1
+        self.scale = 0.7
+        self.image = pygame.transform.scale(self.image,
+            (int(self.image.get_width() * self.scale), int(self.image.get_height() * self.scale)))
 
-        self.pos_x = position[0] - (self.image.get_width() / 2 * self.scale)
-        self.pos_y = position[1] - (self.image.get_height() / 2 * self.scale)
+        self.pos_x = position[0] - (self.image.get_width() / 2)
+        self.pos_y = position[1] - (self.image.get_height() / 2)
 
         self.vel_x = 0
         self.vel_y = 0
